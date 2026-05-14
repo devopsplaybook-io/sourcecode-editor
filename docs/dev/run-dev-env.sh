76fd31d5947225de0731e947d0da8c692024db22
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2026-05-15 06:20:29 +0800

    wip

--- docs/dev/run-dev-env.sh
+++ docs/dev/run-dev-env.sh
@@ -13,28 +13,34 @@ if [ -f "${REPO_DIR}/docs/dev/env.sh" ]; then
 fi
 
 
-# Server
-cd "${REPO_DIR}/sourcecode-editor-server"
-if [ ! -f package-lock.json ]; then
-    rm -fr node_modules
-    npm install
-fi
-if [ ! -d node_modules ]; then
-    npm ci
-fi
+# DB
+cd "${REPO_DIR}/docs/dev/postgres"
+docker compose down || true
+docker compose up -d || true
 
-# Web
-cd "${REPO_DIR}/sourcecode-editor-web"
-if [ ! -f package-lock.json ]; then
-    rm -fr node_modules
-    npm install
-fi
-if [ ! -d node_modules ]; then
-    npm ci
-fi
+install_dependencies_in_folder() {
+	local target_dir="$1"
+
+	echo "==== Installing dependencies in: ${target_dir}"
+
+	cd "${target_dir}"
+
+	if [ ! -f package-lock.json ]; then
+		rm -fr node_modules
+		npm install
+	fi
+	if [ ! -d node_modules ]; then
+		npm ci
+	fi
+}
+
+for dir in "${REPO_DIR}"/*; do
+	if [[ -d "${dir}" && -f "${dir}/package.json" ]]; then
+		install_dependencies_in_folder "${dir}"
+	fi
+done
 
 # Start
 cd "${REPO_DIR}"
 pm2 start ecosystem.config.js --env development
 pm2 logs
-# pm2 logs sourcecode-editor-server
